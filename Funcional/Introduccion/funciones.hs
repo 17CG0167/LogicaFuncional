24641be48807bbf188c76de50a39863e1d044546
17CG0167 <hector.skate99gmail.com>
2020-11-05 23:50:12 -0700

    Primeras Funciones en Haskel

---
+++ Funcional/Introduccion/funciones.hs
@@ -0,0 +1,8 @@
+-- ¿Cómo hacer una suma?
+-- Variables y funciones no pueden empezar con mayuscula
+-- ghc -o funciones ejecutable ./funciones.hs
+-- ghci entrar al programa prelude
+
+suma a b = a + b
+division a b = a `div` b
+pregunta a = not a
