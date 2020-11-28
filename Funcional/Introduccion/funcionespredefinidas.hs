10c8ce250939fdb8669e5c237ab89fa37fb69447
17CG0167 <hector.skate99gmail.com>
2020-11-28 01:30:43 -0700

    practica funciones predefinidas

---
+++ Funcional/Introduccion/funcionespredefinidas.hs
@@ -0,0 +1,38 @@
+
+-- Variables y funciones no pueden empezar con MAYUSCULA
+-- ghc -o funciones ejecutable ./funciones.hs
+-- ghci entrar al programa prelude
+-- :load 
+
+--1. abs
+funcion_1 x = abs x
+--2. ceiling
+funcion_2 x = ceiling x
+--3. succ x
+funcion_3 x = succ x
+--4. min x y
+funcion_4 x y = min x y
+--5. max x y
+funcion_5 x y = max x y
+--6. div x y
+funcion_6 x y = div x y
+--7. mod x y
+funcion_7 x y = mod x y
+--8. even x
+funcion_8 x = even x
+--9 floor x
+funcion_9 x = floor x
+--10. gcd x y
+funcion_10 x y = gcd x y
+--11. not x
+funcion_11 x = not x
+--12. odd x (impar)
+funcion_12 x = odd x
+--13. succ x (estaba repetido (?))
+funcion_13 x = succ x
+--14. signum x
+funcion_14 x = signum x
+--15. snd(x,y)
+funcion_15 x y = snd(x,y)
+--16. signum x (estaba repetido (?)x2)
+funcion_16 x = signum x
